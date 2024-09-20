7be9b98d15521a49f31e160149acc7a6f272bfd0
unknown <alifarizalgg24@gmail.com>
2024-09-20 17:49:04 +0700

    home & pagenav

--- resources/views/pagenav.blade.php
+++ resources/views/pagenav.blade.php
@@ -1,2 +1,44 @@
+<script>
+    <?php
+
+    $file = 1;
+    $data = 1;
+
+    ?>
+</script>
+
+
 @include('css')
-<h1>test</h1>
+<div class="crud-layout">
+    <div class="main-title">
+        <div class="arrow"><img src="{{asset('Vector.png')}}" alt=""></div>
+    <div class="title">
+        <h1 class="text-gradient-orange-500 to-green-400">Data KTP Pribadi</h1>
+    </div>
+</div>
+
+<div class="btn-upload">
+    @if ($file == 0)
+    <a href="#" class="btn blue">
+        Upload Foto/File
+    </a>
+    <div class="jarak">
+        <a href="#" class="btn orange">
+        Validasi Data
+     </a>
+    @else
+    <div class="user">
+        <img src="{{asset('contohktp.png')}}" alt=""/>
+        <a href="" class="btn green">Ubah</a>
+    </div>
+
+    </div>
+    
+  </div>
+  <div class="tabeluser">
+  @include('tabeldata')
+  @endif
+</div>
+
+</div>
+
---
+++ resources/views/tabeldata.blade.php
@@ -0,0 +1,34 @@
+@include('css')
+
+
+<table>
+    <tr>
+        <th>NIK</th>
+        <th>Nama Lengkap</th>
+        <th>Tempat, Tanggal Lahir</th>
+        <th>Jenis Kelamin</th>
+        <th>Alamat</th>
+        <th>Agama</th>
+        <th>Status Perkawinan</th>
+        <th>Pekerjaan</th>
+        <th>Kewarganegaraan</th>
+        <th>Keberlakuan</th>
+        <th>Aksi</th>
+    </tr>
+    <tr>
+        <td>3273210780060003</td>
+        <td>Kainzha Resha Sulistyawati</td>
+        <td>Bekasi, 40 September 2001</td>
+        <td>Perempuan</td>
+        <td>Komp. GBA 2 Blok J8 No 90</td>
+        <td>Islam</td>
+        <td>Belum kawin</td>
+        <td>Pelajar</td>
+        <td>WNI</td>
+        <td>Seumur Hidup</td>
+        <td>
+            <img src="{{asset('editt.png')}}" alt="">
+            <img src="{{asset('trash.png')}}" alt="">
+        </td>
+    </tr>
+</table>
