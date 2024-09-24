f7ef8faefddee96ad2196687eae24c7b4299b78f
unknown <alifarizalgg24@gmail.com>
2024-09-24 14:19:01 +0700

    adding geoloc

--- app/Http/Controllers/IdentitasController.php
+++ app/Http/Controllers/IdentitasController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Identitas;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Storage;
+
 
 class IdentitasController extends Controller
 {
@@ -24,17 +26,55 @@ class IdentitasController extends Controller
     {
         $request->validate([
             'nik' => 'required|string|max:255',
-            // 'image' => 'required|file'
+            'nama_lengkap' => 'required|string|max:255',
+            'tempat_tanggal_lahir' => 'required|string|max:255',
+            'jenis_kelamin' => 'required|string|max:255',
+            'alamat' => 'required|string|max:255',
+            'rt_rw' => 'required|integer|max:255',
+            'kel_desa' => 'required|string|max:255',
+            'kecamatan' => 'required|string|max:255',
+            'agama' => 'required|string|max:255',
+            'status_perkawinan' => 'required|string|max:255',
+            'pekerjaan' => 'required|string|max:255',
+            'kewarganegaraan' => 'required|string|max:255',
+            'berlaku_hingga' => 'required|string|max:255',
+            'file' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            
         ]);
+
+        $file = $request->file('file');
+        $filePath = 'uploads/' . time() . '_' . $file->getClientOriginalName();
+        Storage::disk('minio')->put($filePath, file_get_contents($file));
+
+        $endpoint = env('AWS_ENDPOINT'); 
+        $bucket = env('AWS_BUCKET'); 
+        $url = $endpoint . '/' . $bucket . '/' . $filePath;
+
+        session()->flash('file_url', $url);
 
         $nikEnkripsi = Crypt::encryptString($request->nik);
 
         // $path = $request->file('image')->store('images', 's3');
 
-        Identitas::create(array_merge($request->all(), [
-            'nik' => $nikEnkripsi,
-            // 'image' => $path
-        ]));
+        $identitas = new Identitas();
+
+        $identitas->nik = $nikEnkripsi;
+        $identitas->nama_lengkap = $request->nama_lengkap;
+        $identitas->tempat_tanggal_lahir = $request->tempat_tanggal_lahir;
+        $identitas->jenis_kelamin = $request->jenis_kelamin;
+        $identitas->alamat = $request->alamat;
+        $identitas->rt_rw = $request->rt_rw;
+        $identitas->kel_desa = $request->kel_desa;
+        $identitas->kecamatan = $request->kecamatan;
+        $identitas->agama = $request->agama;
+        $identitas->status_perkawinan = $request->status_perkawinan;
+        $identitas->pekerjaan = $request->pekerjaan;
+        $identitas->kewarganegaraan = $request->kewarganegaraan;
+        $identitas->berlaku_hingga = $request->berlaku_hingga;
+        $identitas->foto_ktp = $url;
+        $identitas->image = $filePath;
+        
+        $identitas->save();
 
         return redirect()->route('pagenav')->with('success', 'Data has been added successfully!');
     }
@@ -48,7 +88,38 @@ class IdentitasController extends Controller
     {
         $user = Identitas::find($id);
 
+        $delImg = Identitas::where('id', $id)->value('image');
+        
+        $request->validate([
+            'nik' => 'nullable',
+            'nama_lengkap' => 'required|string|max:255',
+            'tempat_tanggal_lahir' => 'required|string|max:255',
+            'jenis_kelamin' => 'required|string|max:255',
+            'alamat' => 'required|string|max:255',
+            'rt_rw' => 'required|integer|max:255',
+            'kel_desa' => 'required|string|max:255',
+            'kecamatan' => 'required|string|max:255',
+            'agama' => 'required|string|max:255',
+            'status_perkawinan' => 'required|string|max:255',
+            'pekerjaan' => 'required|string|max:255',
+            'kewarganegaraan' => 'required|string|max:255',
+            'berlaku_hingga' => 'required|string|max:255',
+            'file' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            
+        ]);
+
+        if($user->image) {
+            Storage::disk('minio')->delete($delImg);
+        }
+
         $user->update($request->all());
+        $newImg = $request->file('file');
+        $filePath = 'uploads/' . time() . '_' . $newImg->getClientOriginalName();
+        Storage::disk('minio')->put($filePath, file_get_contents($newImg));
+
+        
+
+        
 
         return redirect()->route('pagenav')->with('success', 'Data has been added successfully!');
     }
@@ -56,8 +127,13 @@ class IdentitasController extends Controller
     public function destroy($id)
     {
         $user = Identitas::findOrFail($id);
-        $user->delete();
 
+        $delImg = Identitas::where('id', $id)->value('image');
+        
+        $user->delete();
+        
+        Storage::disk('minio')->delete($delImg);
+        
         return redirect()->route('pagenav')->with('success', 'Data has been added successfully!');
     }
 }
--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -4,15 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class homeController extends Controller
 {
-    public function home()
-    {
-        $dataC = 'Bandung';
+    public function home(Request $request)
+{
+    $userIP = $request->server('REMOTE_ADDR');
+    $response = Http::get("https://ipinfo.io/{$userIP}?token=" . env('IPINFO_API_KEY'));
 
-        return view('home', compact('dataC'));
+    if ($response->successful()) {
+        $locationData = $response->json();
+        Log::info('Location Data: ' . json_encode($locationData)); 
+        $dataC = $locationData['city'] ?? 'Lokasi tidak diketahui';
+    } else {
+        $dataC = 'Lokasi tidak diketahui';
     }
+
+    return view('home', compact('dataC'));
+}
+
     public function user()
     {
         $file = session('file_uploaded', false);
